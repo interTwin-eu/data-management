051896f2c965a2d816610f419f1a769e34885b6a
vrbanecd <dijana.vrbanec@desy.de>
2025-03-04 18:04:56 +0100

    Added Dockerfiles

---
+++ fts-tutorial-dockerfile/ildg.sh
@@ -0,0 +1,3 @@
+echo "ildg=$(oidc-token ildg --scope="openid profile storage.create:/intertwin/ storage.read:/intertwin/ storage.stage:/intertwin/ storage.modify:/intertwin/")"
+
+echo "ildg=$(oidc-token ildg --scope="openid profile storage.create:/intertwin/ storage.read:/intertwin/ storage.stage:/intertwin/ storage.modify:/intertwin/")" 1>&2
